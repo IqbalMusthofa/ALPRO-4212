604d7ee4eecee57ea263a1b1df413e1fccfdeeb3
IqbalMusthofa <128113916+IqbalMusthofa@users.noreply.github.com>
2023-03-17 11:22:37 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,29 +2,18 @@
 
 using namespace std;
 
-void hitungRataRata(int arr[], int n) {
-   float rata_rata, total = 0;
-
-   for(int i = 0; i < n; i++) {
-      total += arr[i];
-   }
-
-   rata_rata = total / n;
-
-   cout << "Rata-rata dari total jumlah array adalah: " << rata_rata << endl;
-}
-
-int main() {
-   int arr[4];
-
-   for(int i = 0; i < 4; i++) {
-      cout << "Masukkan angka ke-" << i+1 << ": ";
-      cin >> arr[i];
-   }
-
-   hitungRataRata(arr, 4);
-
+int main()
+{
+    //kamus
+    int x;
+    int y;
+    int b;
+    //algoritma
+    cout << "masukkan angka pertama =";
+    cin >> x;
+    cout << "masukkan angka kedua =";
+    cin >> y;
+    b= x+y;
+    cout << "hasil penjumlahan diatas adalah =" <<b<<endl;
     return 0;
 }
-
-
